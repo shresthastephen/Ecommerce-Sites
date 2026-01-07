a5067eec87ae99a06edd98b5c18e63851ed5e09b
shresthastephen <stephencrestha07@gmail.com>
2026-01-07 19:23:41 +0545

    first

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind CSS v3 is working 🚀
+      </h1>
+    </div>
+  )
+}
